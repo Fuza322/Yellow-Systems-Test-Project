f240a42f9ab924b14dbbbaaf5ac5a7ced24328d1
Fuza322 <vanya.osipik322@gmail.com>
2021-09-11 16:30:55 +0300

    Add Jogs page styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,54 @@
 import React from "react"
+import {JogItem} from "../common/JogItem/JogItem"
 import style from "./Main.module.scss"
 
 type MainPropsType = {}
 
 export const Main = React.memo((props: MainPropsType) => {
-    return (
-        <>
 
-        </>
+    const jogItems = [
+        {
+            date: "20.12.2017",
+            speed: 15,
+            distance: 10,
+            time: 60
+        },
+        {
+            date: "20.12.2017",
+            speed: 15,
+            distance: 10,
+            time: 60
+        },
+        {
+            date: "20.12.2017",
+            speed: 15,
+            distance: 10,
+            time: 60
+        },
+        {
+            date: "20.12.2017",
+            speed: 15,
+            distance: 10,
+            time: 60
+        },
+        {
+            date: "20.12.2017",
+            speed: 15,
+            distance: 10,
+            time: 60
+        },
+
+    ]
+
+    return (
+        <section className={style.mainBlock}>
+            <div className={style.mainContainer}>
+                {jogItems.map(item => {
+                    return (
+                        <JogItem/>
+                    )
+                })}
+            </div>
+        </section>
     )
 })
--- src/components/common/JogItem/JogItem.tsx
+++ src/components/common/JogItem/JogItem.tsx
@@ -1,12 +1,29 @@
 import React from "react"
+import jogImage from "../../../assets/images/jog/icon.png"
 import style from "./JogItem.module.scss"
 
 type JogItemPropsType = {}
 
 export const JogItem = React.memo((props: JogItemPropsType) => {
     return (
-        <>
-
-        </>
+        <article className={style.jogItemContainer}>
+            <div className={style.jogItemImgContainer}>
+                <img src={jogImage} alt={"jogItemImg"} className={style.jogItemImg}/>
+            </div>
+            <div className={style.jogItemInfo}>
+                <div className={style.jogInfoItem}>
+                    <span className={style.jogInfoData}>20.12.2017</span>
+                </div>
+                <div className={style.jogInfoItem}>Speed:&nbsp;
+                    <span className={style.jogInfoData}>15</span>
+                </div>
+                <div className={style.jogInfoItem}>Distance:&nbsp;
+                    <span className={style.jogInfoData}>10 km</span>
+                </div>
+                <div className={style.jogInfoItem}>Time:&nbsp;
+                    <span className={style.jogInfoData}>60 min</span>
+                </div>
+            </div>
+        </article>
     )
 })
